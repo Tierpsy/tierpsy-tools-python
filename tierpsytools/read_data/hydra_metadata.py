a2e4602a8d59807dc82e688091b560d68b1c30e7
em812 <eleni.minga@gmail.com>
2020-04-23 09:10:28 +0100

    Debugged the function that reads hydra metadata and matches them with the feature matrix. Added the function align_bluelight_conditions which reshapes the feature df to (n_wells_screened) x (n_feat_prestim+n_feat_bluelight+n_feat_poststim) and creates a metadata df that matches the new feature matrix row-by-row.

--- tierpsytools/read_data/hydra_metadata.py
+++ tierpsytools/read_data/hydra_metadata.py
@@ -7,6 +7,7 @@ Created on Tue Apr  7 15:52:14 2020
 """
 
 import pandas as pd
+import pdb
 
 def add_bluelight_label(
         meta, filename_column='imgstore_name', split_string='_', location=3
@@ -59,3 +60,74 @@ def read_hydra_metadata(
     assert newmeta.shape[0] == feat.shape[0]
 
     return feat[feat.columns.difference(feat_id_cols)], newmeta
+
+def align_bluelight_conditions(
+        feat, meta,
+        how = 'outer',
+        return_separate_feat_dfs = False,
+        bluelight_specific_meta_cols = ['imgstore_name',
+                                        'file_id', 'bluelight'],
+        merge_on_cols = ['date_yyyymmdd','imaging_plate_id','well_name']
+        ):
+    """
+    !: is bad well not considered bluelight specific!
+
+    Parameters
+    ----------
+    feat : TYPE
+        DESCRIPTION.
+    meta : TYPE
+        DESCRIPTION.
+    how : TYPE, optional
+        DESCRIPTION. The default is 'outer'.
+    return_separate_feat_dfs : TYPE, optional
+        DESCRIPTION. The default is False.
+    bluelight_specific_meta_cols : TYPE, optional
+        DESCRIPTION. The default is ['imgstore_name',                                        'file_id', 'bluelight'].
+    merge_on_cols : TYPE, optional
+        DESCRIPTION. The default is ['date_yyyymmdd','imaging_plate_id','well_name'].
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+
+    """
+
+    bluelight_conditions = ['prestim', 'bluelight', 'poststim']
+
+    distinct_meta_cols = bluelight_specific_meta_cols
+    shared_meta_cols = meta.columns.difference(distinct_meta_cols+merge_on_cols).to_list()
+
+    feat_names = feat.columns.to_list()
+
+    feat = pd.concat([meta, feat], axis=1)
+
+    feat = feat.set_index(merge_on_cols)
+
+    feat = feat[feat['bluelight']=='prestim'].join(
+        feat[feat['bluelight']=='bluelight'],
+        how=how, lsuffix='', rsuffix='_bluelight').join(
+        feat[feat['bluelight']=='poststim'],
+        how=how, lsuffix='', rsuffix='_poststim')
+
+    feat = feat.rename(
+            columns={ft:ft+'_prestim' for ft in distinct_meta_cols+feat_names})
+
+    feat_cols = ['_'.join([ft,blue]) for blue in bluelight_conditions for ft in feat_names]
+
+    feat.reset_index(drop=False, inplace=True)
+    meta = feat[feat.columns.difference(feat_cols)]
+    feat = feat[feat_cols]
+
+    for col in shared_meta_cols:
+        for blue in bluelight_conditions[1:]:
+            meta.loc[meta[col].isna(), col] = meta.loc[meta[col].isna(), '_'.join([col, blue])]
+            meta = meta.drop(columns=['_'.join([col, blue])])
+
+    if return_separate_feat_dfs:
+        return (feat[[ft for ft in feat.columns if '_prestim' in ft]], \
+            feat[[ft for ft in feat.columns if '_bluelight' in ft]], \
+            feat[[ft for ft in feat.columns if '_poststim' in ft]]), meta
+    else:
+        return feat, meta
