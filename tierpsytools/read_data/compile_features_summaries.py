0e30b67388f1e975fe22664f581483097e7c9ab4
em812 <eleni.minga@gmail.com>
2020-06-13 12:36:01 +0100

    improvements in the compile_features_summaries function

--- tierpsytools/read_data/compile_features_summaries.py
+++ tierpsytools/read_data/compile_features_summaries.py
@@ -54,7 +54,30 @@ def find_fname_summaries(feat_files):
 
     return fname_files
 
-def copy_commented_headers(source_file, dest_file, comment='#'):
+def check_summary_type(feat_files, fname_files):
+    sum_type = []
+    for file in feat_files + fname_files:
+        tmp = ''.join(file.stem.split('_')[2:])
+        tmp = ''.join([l for l in tmp if not l.isdigit()])
+        sum_type.append(tmp)
+    if not all(stype == sum_type[0] for stype in sum_type):
+        raise ValueError('The summary files specified are not of the same type.')
+    return
+
+def _get_fname_files(feat_files):
+    fname_files = []
+    for file in feat_files:
+        with open(file, "r") as fid:
+            header = fid.readline()
+            if not header.startswith('#'):
+                raise ValueError(
+                    'No header found in features summaries file. ' +
+                    'Corresponding filename summaries file cannot be retrieved.')
+            fname_files.append(header.replace('#','').replace(' ', ''))
+    assert len(feat_files) == len(fname_files)
+    return fname_files
+
+def _copy_commented_headers(source_file, dest_file, comment='#'):
     fid = open(source_file, 'r')
     fid_comp = open(dest_file, 'w')
 
@@ -68,24 +91,17 @@ def copy_commented_headers(source_file, dest_file, comment='#'):
     fid_comp.close()
     return
 
-
-def check_summary_type(feat_files, fname_files):
-    sum_type = []
-    for file in feat_files + fname_files:
-        tmp = ''.join(file.stem.split('_')[2:])
-        tmp = ''.join([l for l in tmp if not l.isdigit()])
-        sum_type.append(tmp)
-    if not all(stype == sum_type[0] for stype in sum_type):
-        raise ValueError('The summary files specified are not of the same type.')
-    return
-
-def compile_columns(file_list):
+def _compile_columns(file_list):
+    from pandas.errors import EmptyDataError
 
     # Read one line from each dataframe
     dfsamples = []
     for file in file_list:
-        dfsamples.append(pd.read_csv(
+        try:
+            dfsamples.append(pd.read_csv(
                 file, index_col=None, header=0, nrows=1, comment='#'))
+        except EmptyDataError:
+            raise EmptyDataError('Summary file empty: \n{}'.format(file))
 
     # Compare number of columns
     ncols = [df.shape[1] for df in dfsamples]
@@ -97,16 +113,49 @@ def compile_columns(file_list):
 
     return columns
 
-def write_columns_header(columns, filename):
+def _write_columns_header(columns, filename):
     with open(filename,'a') as fid:
         fid.write(','.join(columns)+'\n')
     return
 
-def compile_tierpsy_summaries(feat_files, fname_files, compiled_feat_file, compiled_fname_file):
+def _sort_columns(df, columns):
+    from numpy import nan
+    missing = [col for col in columns if col not in df.columns]
+    if missing:
+        for col in missing:
+            df[col] = nan
+    return df[columns]
 
+def compile_tierpsy_summaries(
+        feat_files, compiled_feat_file, compiled_fname_file, fname_files=None):
+    """
+    Reads feature summaries from different days of the same experiment and
+    compiled them to a single feature summaries file and a single filenames
+    summaries file.
+
+    Parameters
+    ----------
+    feat_files : list
+        The list of feature summaries files to compile.
+    compiled_feat_file : path
+        The full path to the compiled features summaries file.
+    compiled_fname_file : path
+        The full path to the compiled filenames summaries file.
+    fname_files : list (optional)
+        The list of filenames summaries files that correspond to the feat_files
+        list. If None, the filenames summaries names will be read from the
+        header of the feat_files.
+    Raises
+    ------
+    ValueError
+        If the compiled_feat_file or the compiled_fname_file already exist.
+
+    Returns
+    -------
+    None.
+
+    """
     import pdb
-
-    check_summary_type(feat_files, fname_files)
 
     if compiled_feat_file.exists() or compiled_fname_file.exists():
         raise ValueError(
@@ -115,24 +164,32 @@ def compile_tierpsy_summaries(feat_files, fname_files, compiled_feat_file, compi
             'remove or rename the existing files.'
             )
 
-    copy_commented_headers(feat_files[0], compiled_feat_file)
-    copy_commented_headers(fname_files[0], compiled_fname_file)
+    if fname_files is None:
+        fname_files = _get_fname_files(feat_files)
+
+    check_summary_type(feat_files, fname_files)
+
+    _copy_commented_headers(feat_files[0], compiled_feat_file)
+    _copy_commented_headers(fname_files[0], compiled_fname_file)
 
     ## Get all the columns from the different files:
-    feat_columns = compile_columns(feat_files)
-    fname_columns = compile_columns(fname_files)
+    feat_columns = _compile_columns(feat_files)
+    fname_columns = _compile_columns(fname_files)
 
-    write_columns_header(feat_columns, compiled_feat_file)
-    write_columns_header(fname_columns, compiled_fname_file)
+    _write_columns_header(feat_columns, compiled_feat_file)
+    _write_columns_header(fname_columns, compiled_fname_file)
 
-    # Append all the dataframes to files
+    # Append dataframes to files
     counter = 0
     for i, (feat_fl, fname_fl) in enumerate(zip(feat_files, fname_files)):
-        print('Appending dataframes to compiled files...'); st_time = time()
+        print('Appending dataframes to compiled files {}/{}...'.format(i+1, len(feat_files)))
+        print(feat_fl)
+        print(fname_fl)
+        st_time = time()
         fnames = pd.read_csv(fname_fl, header=0, comment='#')
         nfiles = fnames['file_id'].max() + 1
         fnames['file_id'] = fnames['file_id'] + counter
-        fnames = pd.concat([pd.DataFrame(columns=fname_columns), fnames], axis=0, sort=False)
+        fnames = _sort_columns(fnames, fname_columns)
         with open(compiled_fname_file, 'a') as fid:
             fnames.to_csv(fid, header=False, index=False)
 
@@ -141,7 +198,7 @@ def compile_tierpsy_summaries(feat_files, fname_files, compiled_feat_file, compi
         except:
             pdb.set_trace()
         feat['file_id'] = feat['file_id'] + counter
-        feat = pd.concat([pd.DataFrame(columns=feat_columns), feat], axis=0, sort=False)
+        feat = _sort_columns(feat, feat_columns)
         with open(compiled_feat_file, 'a') as fid:
             feat.to_csv(fid, header=False, index=False)
         print('Done in {:.2f} sec.'.format(time()-st_time))
