6d4901e9ca5e69e0bdb74995119163ef52cc2382
em812 <eleni.minga@gmail.com>
2020-08-05 09:56:58 +0100

    Updated helper to reflect last commits in MIL modules.

--- tierpsytools/analysis/helper.py
+++ tierpsytools/analysis/helper.py
@@ -7,18 +7,32 @@ Created on Wed Jul 15 21:12:26 2020
 """
 
 import numpy as np
+from inspect import isfunction, isclass
 
-def _check_if_classifscorer(scorer):
-    from inspect import isfunction
+def _get_multi_sclassifscorers(scorer):
+    if not isinstance(scorer, list):
+        scorer = [scorer]
+
+    score_dict = dict()
+
+    for score in scorer:
+        scorer_obj =  _get_classifscorer(score)
+        score_dict[scorer_obj.name] = scorer_obj
+
+    return score_dict
+
+def _get_classifscorer(scorer):
     from tierpsytools.analysis.scorers import ClassifScorer
 
     if scorer is None:
-        scorer = ClassifScorer()
+        scorer = ClassifScorer(scorer='accuracy')
     elif isfunction(scorer):
         raise Exception('The scorer input must be a ClassifScorer object. '+
                         'Define a ClassifScorer object with the scorer '+
                         'function.')
-    elif not isinstance(scorer, ClassifScorer):
+    elif isinstance(scorer, str):
+        scorer = ClassifScorer(scorer=scorer)
+    elif not isclass(type(ClassifScorer())):
         raise Exception('scorer input not recognised. The scorer must be a '+
                         'ClassifScorer object or None.')
     return scorer
