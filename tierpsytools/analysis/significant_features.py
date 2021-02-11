3ea92c7468a6aba9de93f04ba64410787cece01f
em812 <eleni.minga@gmail.com>
2021-02-11 12:48:20 +0000

    Modified read_data/hydra_metadata to use file paths instead of dataframes. Added statistical tests module for general use and modiled filter_compounds modules accordingly. Minor improvement in plot_confusion_matrix function.

--- tierpsytools/analysis/significant_features.py
+++ tierpsytools/analysis/significant_features.py
@@ -430,3 +430,214 @@ if __name__=="__main__":
         close_after_plotting=False)
 
     top_feat3, scores3 = top_feat_in_PCs(X, pc=0, scale=True, k=5, feat_names=['X1','X2','X3','X4','X5'])
+
+#%%
+def _univariate_tests(
+        X, y, control='N2', test='ANOVA',
+        comparison_type='multiclass', n_jobs=-1):
+    """
+    Test whether a single compound has siginificant effects compared to the
+    control using univariate tests for each feature.
+    Each feature is tested using one of the methods 'ANOVA', 'Kruskal-Wallis',
+    'Mann-Whitney test' or 't-test'.
+    The pvalues from the different features are corrected for multiple
+    comparisons using the multitest methods of statsmodels.
+
+    Parameters
+    ----------
+    X : TYPE
+        DESCRIPTION.
+    y : TYPE
+        DESCRIPTION.
+    comparison_type : TYPE, optional
+        DESCRIPTION. The default is 'multiclass'.
+    control : float, optional. The default is .0.
+        The drug_dose entry for the control points.
+        Must provide control dose if the comparison_type is 'binary_each_group'.
+    test : TYPE, optional
+        DESCRIPTION. The default is 'ANOVA'.
+    multitest_method : TYPE, optional
+        DESCRIPTION. The default is 'fdr_by'.
+    fdr : TYPE, optional
+        DESCRIPTION. The default is 0.05.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+    TYPE
+        DESCRIPTION.
+
+    """
+    from scipy.stats import kruskal, mannwhitneyu, f_oneway, ttest_ind
+    from functools import partial
+
+    if not np.isin(control, np.array(y)):
+        raise ValueError('control not found in the y array.')
+
+    if test.startswith('Wilkoxon') or test == 't-test':
+        if comparison_type=='multiclass' and np.unique(y).shape[0]>2:
+            raise ValueError(
+                """
+            The Wilkoxon rank sum test cannot be used to compare between
+            more than two groups. Use a different test or the
+            binary_each_dose comparison_method instead.
+                """)
+        else:
+            comparison_type = 'binary_each_group'
+
+    if not isinstance(X, pd.DataFrame):
+        X = pd.DataFrame(X)
+
+    # Local function for parallel processing of univariate tests for each drug
+    def stats_test(X, y, test, **kwargs):
+        from joblib import Parallel, delayed
+
+        def _one_fit(ift, samples, **kwargs):
+            samples = [s[~np.isnan(s)] for s in samples if not all(np.isnan(s))]
+            if len(samples)<2:
+                return ift, (np.nan, np.nan)
+            return ift, test(*samples, **kwargs)
+
+        parallel = Parallel(n_jobs=n_jobs, verbose=True)
+        func = delayed(_one_fit)
+
+        try:
+            res = parallel(
+                func(ift, [sample[:,ift]
+                      for sample in [np.array(X[y==iy]) for iy in np.unique(y)]],
+                     **kwargs)
+                for ift in range(X.shape[1]))
+        except:
+            pdb.set_trace()
+
+        order = [ift for ift,(r,p) in res]
+        rs = np.array([r for ift,(r,p) in res])
+        ps = np.array([p for ift,(r,p) in res])
+
+        return rs[order], ps[order]
+
+    # Create the function that will test every feature of a given drug
+    if test == 'ANOVA':
+        func = partial(stats_test, test=f_oneway)
+    elif test.startswith('Kruskal'):
+        func = partial(stats_test, test=kruskal, nan_policy='raise')
+    elif test.startswith('Mann-Whitney'):
+        func = partial(stats_test, test=mannwhitneyu)
+    if test == 't-test':
+        func = partial(stats_test, test=ttest_ind)
+
+    # For each dose get significant features
+    if comparison_type=='multiclass':
+        stats, pvals = func(X, y)
+        pvals = pd.DataFrame(pvals.T, index=X.columns, columns=test)
+        stats = pd.DataFrame(stats.T, index=X.columns, columns=test)
+
+    elif comparison_type=='binary_each_group':
+        groups = np.unique(y[y!=control])
+
+        pvals=[]
+        stats=[]
+        for igrp, grp in enumerate(groups):
+
+            mask = np.isin(y,[control, grp])
+            _stats, _pvals = func(X[mask], y[mask])
+
+            pvals.append(_pvals)
+            stats.append(_stats)
+        pvals = pd.DataFrame(np.array(pvals).T, index=X.columns, columns=groups)
+        stats = pd.DataFrame(np.array(stats).T, index=X.columns, columns=groups)
+    else:
+        raise ValueError('Comparison type not recognised.')
+
+    return stats, pvals
+
+def _get_effect_sizes(
+        X, y, control='N2',
+        test='ANOVA', comparison_type='multiclass',
+        n_jobs=-1):
+    """
+    Test whether a single compound has siginificant effects compared to the
+    control using univariate tests for each feature.
+    Each feature is tested using one of the methods 'ANOVA', 'Kruskal-Wallis',
+    'Mann-Whitney' or 't-test'.
+    The pvalues from the different features are corrected for multiple
+    comparisons using the multitest methods of statsmodels.
+
+    Parameters
+    ----------
+    X : TYPE
+        DESCRIPTION.
+    drcomparison_variableug_dose : TYPE
+        DESCRIPTION.
+    comparison_type : TYPE, optional
+        DESCRIPTION. The default is 'multiclass'.
+    control : float, optional. The default is .0.
+        The drug_dose entry for the control points.
+        Must provide control dose if the comparison_type is 'binary_each_dose'.
+    test : TYPE, optional
+        DESCRIPTION. The default is 'ANOVA'.
+    multitest_method : TYPE, optional
+        DESCRIPTION. The default is 'fdr_by'.
+    fdr : TYPE, optional
+        DESCRIPTION. The default is 0.05.
+
+    Raises
+    ------
+    ValueError
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+    TYPE
+        DESCRIPTION.
+
+    """
+
+    if not np.isin(control, np.array(y)):
+        raise ValueError('control not found in the comparison_variable array.')
+
+    if comparison_type=='multiclass' and np.unique(y).shape[0]>2:
+        if test.startswith('Mann') or test == 't-test':
+            raise ValueError(
+                """
+            The Mann-Whitney test cannot be used to compare between
+            more than two groups. Use a different test or the
+            binary_each_dose comparison_method instead.
+                """)
+
+    if not isinstance(X, pd.DataFrame):
+        X = pd.DataFrame(X)
+
+    groups = np.unique(y[y!=control])
+
+    # For each dose get significant features
+    if test=='ANOVA' and comparison_type=='multiclass':
+        effect = pd.Series(index=X.columns)
+        samples = [x for ix,x in X.groupby(by=y)]
+        for ft in X.columns:
+            effect[ft] = EffectSize_ANOVA(*[s.loc[~s[ft].isna(), ft] for s in samples])
+        effect = pd.DataFrame(effect, columns=['_'.join([test,'effect_size'])])
+    else:
+        if test=='Mann-Whitney' or test=='Kruskal-Wallis':
+            func = cliffsDelta
+        elif test=='ANOVA':
+            func = EffectSize_ANOVA
+        elif test=='t-test':
+            func = cohen_d
+
+        effect = pd.DataFrame(index=X.columns, columns=groups)
+        for igrp, grp in enumerate(groups):
+            mask = np.isin(y,[control, grp])
+            samples = [x for ix,x in X[mask].groupby(by=y)]
+            for ft in X.columns:
+                effect.loc[ft, grp] = func(*[s.loc[~s[ft].isna(), ft] for s in samples])
+
+    return effect
